e5f0f43f9a72033a6e935483fa6baa2328d09ace
b11isa <isaburkhonovnit@gmail.com>
2023-08-23 21:52:00 +0500

    hw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import time
 import os
+
 url = "https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0"
 a = requests.get(url)
 text = BeautifulSoup(a.text, 'html.parser')
